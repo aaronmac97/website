6333c314338f30295642ea0888c1c96371781162
aaronmac97 <105774691+aaronmac97@users.noreply.github.com>
2024-08-08 19:23:51 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// Typed js
+var typed6 = new Typed('.typed', {
+    strings: ['Website in progress, <br> \n ....'],
+    typeSpeed: 100,
+    backSpeed: 0,
+    cursorChar: '|',
+    loop: true
+  
+  });
+// aos 
+  AOS.init()
